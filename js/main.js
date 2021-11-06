f3e9a19744dfd97fff4c4231b432d8b9b638d3b6
Alison Leme <87146432+AlisonLetos@users.noreply.github.com>
2021-11-06 01:54:13 -0300

    Add Local Storage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const Main = {
+
+    tasks: [],
+
     init: function() {
         this.cacheSelectors();
         this.bindEvents();
+        this.getStoraged();
+        this.buildTasks();
     },
 
     // função para selecionar os elementos html e armazena-los em uma váriavel.
@@ -17,27 +22,86 @@ const Main = {
         const self = this;
         
         this.$checkButtons.forEach(function(button) {
-            button.addEventListener('click', self.Events.checkButton_click);
+            button.addEventListener('click', self.Events.checkButton_click.bind(self));
         });
 
         this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this);
 
         this.$deleteButtons.forEach(function(button) {
-            button.onclick = self.Events.deleteButton_click;
+            button.onclick = self.Events.deleteButton_click.bind(self);
         })
+    },
+
+    // função responsalvel por fazer o get das tarefas
+    getStoraged: function() {
+        const tasks = localStorage.getItem('tasks');
+
+        if(tasks)
+        {
+            this.tasks = JSON.parse(tasks);
+        }
+        else
+        {
+            localStorage.setItem('tasks', JSON.stringify([]));
+        }
+    },
+
+    //função para retornar o HTML montado com a tarefa do usuario
+    getTaskHtml: function(task, done) {
+        return `
+            <li class="task">
+                <div class="check" data-done="${done}"></div>
+                <span>${task}</span>
+                <button class="btn-delete" data-task="${task}"></button>
+            </li>
+        `;
+    },
+
+    // função para pegar as tarefas e montar na tela.
+    buildTasks: function() {
+        let html = '';
+        this.tasks.forEach(task => {
+            html += this.getTaskHtml(task.task, task.done);
+        });
+
+        this.$toDoList.innerHTML = html;
+
+        this.cacheSelectors();
+        this.bindEvents();
     },
 
     Events: {
         checkButton_click: function(element) {
-            const check = element.target;
-            const isDone = check.classList.contains('done');
+            const done = element.target.parentElement
+            const taskSpan = done.querySelector('span').innerHTML
+            let isDone = element.target.getAttribute('data-done');
 
-            if(!isDone)
+            if(isDone === 'false')
             {
-                return check.classList.add('done');
-            }
+                isDone = true
+                this.tasks.forEach(task => {
+                    if(task.task === taskSpan)
+                    {
+                        task.done = true;
+                        element.target.setAttribute('data-done', 'true');
+                    }
+                });
 
-            check.classList.remove('done');
+                localStorage.setItem('tasks', JSON.stringify(this.tasks));
+            }
+            else
+            {
+                isDone = false;
+                this.tasks.forEach(task => {
+                    if(task.task === taskSpan)
+                    {
+                        task.done = false;
+                        element.target.setAttribute('data-done', 'false');
+                    }
+                });
+    
+                localStorage.setItem('tasks', JSON.stringify(this.tasks));
+            }
         },
 
         inputTask_keypress: function(event) {
@@ -46,23 +110,34 @@ const Main = {
 
             if(key === 'Enter' && value !== '')
             {
-                this.$toDoList.innerHTML += `
-                    <li class="task">
-                        <div class="check"></div>
-                        <span>${value}</span>
-                        <button class="btn-delete"></button>
-                    </li>
-                `;
+                this.$toDoList.innerHTML += this.getTaskHtml(value, false);
 
                 event.target.value = '';
 
                 this.cacheSelectors();
                 this.bindEvents();
+
+                const savedTasks = localStorage.getItem('tasks');
+                const savedTasksObj = JSON.parse(savedTasks);
+                const obj = [
+                    ...savedTasksObj, // spread operator = jogar todas as tarefas que já estão salvas
+                    {task: value, done: false},
+                ];
+
+                this.tasks = obj;
+                localStorage.setItem('tasks', JSON.stringify(obj));
+
             }
         },
 
         deleteButton_click: function(element) {
             const li = element.target.parentElement;
+            const value = element.target.dataset['task'];
+
+            const newTasksState = this.tasks.filter(task => task.task !== value);
+
+            localStorage.setItem('tasks', JSON.stringify(newTasksState));
+            this.tasks = newTasksState;
 
             li.classList.add('removed');
 
